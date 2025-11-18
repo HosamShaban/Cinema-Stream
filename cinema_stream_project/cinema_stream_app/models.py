f6fa24b76ac17117df8afe8b6d16e22bc968a76f
Hosam Shaban <61704449+HosamShaban@users.noreply.github.com>
2025-11-18 07:23:36 +0200

    create Genre table

--- cinema_stream_project/cinema_stream_app/models.py
+++ cinema_stream_project/cinema_stream_app/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=60, unique=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
