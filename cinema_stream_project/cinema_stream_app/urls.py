7f9b44ad47ff3660e227a27757859469a605f80e
Hosam Shaban <61704449+HosamShaban@users.noreply.github.com>
2025-11-21 06:50:22 +0200

    create main path urls

--- cinema_stream_project/cinema_stream_app/urls.py
+++ cinema_stream_project/cinema_stream_app/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name='home')
+    path('browse/', views.browse, name='browse'),
+    path('movie/<slug:slug>/', views.movie_detail, name='movie_detail'),
 ]
