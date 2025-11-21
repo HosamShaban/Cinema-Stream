4f87fd8d328727115ea03f47818af611b35df2c5
Hosam Shaban <61704449+HosamShaban@users.noreply.github.com>
2025-11-21 05:19:16 +0200

    create browse

--- cinema_stream_project/cinema_stream_app/views.py
+++ cinema_stream_project/cinema_stream_app/views.py
@@ -13,3 +13,27 @@ def home(request):
         'page_title': 'Cinema Stream - Home'
     }
     return render(request, 'home.html', context)
+
+def browse(request):
+    genres = models.Genre.objects.all()
+
+    query = request.GET.get('q')
+    genre = request.GET.get('genre')
+    year = request.GET.get('year')
+    ctype = request.GET.get('type')
+
+    if query:
+        movies = models.search_movies(query)
+    else:
+        movies = models.filter_movies(genre=genre, year=year, content_type=ctype)
+
+    context = {
+        'movies': movies,
+        'genres': genres,
+        'current_genre': genre,
+        'current_year': year,
+        'current_type': ctype,
+        'query': query,
+        'page_title': 'Browse Movies & Series'
+    }
+    return render(request, 'browse.html', context)
