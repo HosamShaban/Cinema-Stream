e95a68cfca1613b118b6c766a8a5a450aed950b7
Hosam Shaban <61704449+HosamShaban@users.noreply.github.com>
2025-11-21 06:51:14 +0200

    create auth urls

--- cinema_stream_project/cinema_stream_app/urls.py
+++ cinema_stream_project/cinema_stream_app/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
     path('browse/', views.browse, name='browse'),
     path('movie/<slug:slug>/', views.movie_detail, name='movie_detail'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile, name='profile'),
 ]
