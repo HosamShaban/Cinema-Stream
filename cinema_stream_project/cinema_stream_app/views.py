1cae57d75945816502ea5deca7f369274266d29c
Hosam Shaban <61704449+HosamShaban@users.noreply.github.com>
2025-11-21 05:18:55 +0200

    create home view

--- cinema_stream_project/cinema_stream_app/views.py
+++ cinema_stream_project/cinema_stream_app/views.py
@@ -3,4 +3,13 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from . import models
 
+def home(request):
+    trending = models.get_trending_movies()
+    latest = models.get_all_movies()[:12]
 
+    context = {
+        'trending_movies': trending,
+        'latest_movies': latest,
+        'page_title': 'Cinema Stream - Home'
+    }
+    return render(request, 'home.html', context)
