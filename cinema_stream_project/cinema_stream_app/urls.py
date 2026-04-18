3d1cd38b064a2076cb7833294defbb6d1888417c
Hosam Shaban <61704449+HosamShaban@users.noreply.github.com>
2026-04-18 21:32:11 +0200

    update

--- cinema_stream_project/cinema_stream_app/urls.py
+++ cinema_stream_project/cinema_stream_app/urls.py
@@ -29,13 +29,13 @@ urlpatterns = [
     path('api/auth/logout/',   views.api_logout,   name='api_logout'),
     path('api/movie/<slug:slug>/',  views.api_movie_detail,  name='api_movie_detail'),
     path('api/series/<slug:slug>/', views.api_series_detail, name='api_series_detail'),
-   path('api/review/', views.api_reviews, name='api_reviews'),
-path('api/review/submit/', views.api_post_review, name='api_post_review'),
-path('api/review/<int:review_id>/delete/', views.api_delete_review, name='api_delete_review'),
-path('api/user-reviews/', views.api_user_reviews, name='api_user_reviews'),
-path('api/auth/edit-profile/', views.api_edit_profile, name='api_edit_profile'),
-path('api/home/', views.api_home, name='api_home'),
-path('api/browse/', views.api_browse, name='api_browse'),
+    path('api/review/', views.api_reviews, name='api_reviews'),
+    path('api/review/submit/', views.api_post_review, name='api_post_review'),
+    path('api/review/<int:review_id>/delete/', views.api_delete_review, name='api_delete_review'),
+    path('api/user-reviews/', views.api_user_reviews, name='api_user_reviews'),
+    path('api/auth/edit-profile/', views.api_edit_profile, name='api_edit_profile'),
+    path('api/home/', views.api_home, name='api_home'),
+    path('api/browse/', views.api_browse, name='api_browse'),
 
 
 ]
